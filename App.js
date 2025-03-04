ad49ef72dd61f5f996d530737f10069c3b37bc1b
gjh70 <147407064+gjh70@users.noreply.github.com>
2025-03-05 01:03:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from "./src/screens/LoginScreen";
-import FarmerDashboard from "./src/screens/FarmerDashboard"; // Create this file
-import IndustryDashboard from "./src/screens/IndustryDashboard"; // Create this file
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen.js'; 
 
 const Stack = createStackNavigator();
 
@@ -12,8 +10,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="FarmerDashboard" component={FarmerDashboard} />
-        <Stack.Screen name="IndustryDashboard" component={IndustryDashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
