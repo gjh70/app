0d355e123e33fa7d2fcd3e3d5b52887dd818eaba
gjh70 <147407064+gjh70@users.noreply.github.com>
2025-03-09 13:21:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen.js';
 import IndustryHome from './src/screens/IndustryHome.js';
 import IndustryTrans from './src/screens/IndustryTrans.js';
+import IndustryHelp from './src/screens/IndustryHelp.js';
+import IndustryProfile from './src/screens/IndustryProfile.js';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="IndustryHome" component={IndustryHome} />
         <Stack.Screen name="IndustryTrans" component={IndustryTrans} />
+        <Stack.Screen name="IndustryHelp" component={IndustryHelp} />
+        <Stack.Screen name="IndustryProfile" component={IndustryProfile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
